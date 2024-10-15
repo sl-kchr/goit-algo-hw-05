0b65ed9aa4a468bd838bbfd2c6b81107edde1fc7
sl-kchr <kucherslavka@gmail.com>
2024-10-15 15:38:39 +0200

    1

---
+++ ex1.py
@@ -0,0 +1,14 @@
+def caching_fibonacci():
+    chace = {}
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n in chace:
+            return chace[n]
+        chace[n] = fibonacci(n -1 ) + fibonacci(n - 2)
+        return chace[n]        
+    return  fibonacci
+fib = caching_fibonacci()
+print(fib(15)) 
